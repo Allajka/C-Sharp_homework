37cc87251e505d6c0f78d8024a0bd788685ca191
allajka <allajka94@gmail.com>
2022-08-08 17:29:31 +0300

    Homework02

---
+++ homework02/example001/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите порядковый номер дня недели:  ");
+int userDay = Convert.ToInt32(Console.ReadLine());
+string[] weekDays = new string[] { "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday ", "Saturday", "Sunday" };
+
+
+while (userDay > 7 || userDay <= 0) 
+{
+    Console.Write("Введите порядковый номер дня недели от 1 до 7:  ");
+    userDay = Convert.ToInt32(Console.ReadLine());
+}
+
+
+if (userDay == 6 || userDay == 7) Console.WriteLine($"{weekDays[userDay-1]} выходной"); 
+else Console.WriteLine($"{weekDays[userDay-1]} будний день");
+
