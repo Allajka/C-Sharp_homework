f0be7d7b38a80b8323fd61ed2760a2f1805a96e2
allajka <allajka94@gmail.com>
2022-08-08 17:30:56 +0300

    Homework02

---
+++ homework02/example003/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int checkNumber(int numbers)
+{
+    int amountNumber = 0;
+    while (numbers > 0)
+    {
+        numbers /= 10;
+        amountNumber++;
+    }
+    return amountNumber;
+}
+
+int number = 12;
+int sumNumber=checkNumber(number);
+int control = sumNumber - 3;
+int i = 0;
+
+if (sumNumber >= 3)
+{
+    while (i < control)
+    {
+        number = number / 10;
+        i++;
+    }
+    int thirdNumber = number % 10;
+    Console.WriteLine($"Третья цифра числа {thirdNumber}");
+}
+
+if (sumNumber < 3)
+{
+    Console.WriteLine("Нет третьей цифры");
+}
+
