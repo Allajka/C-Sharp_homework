e4a1a20aedcd7b4024987439b41eff484bbcf3c5
allajka <allajka94@gmail.com>
2022-08-15 11:12:04 +0300

    homework04

---
+++ homework04/example002/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int box = number;
+int sum = 0;
+while (box > 0)
+{ 
+sum += box % 10;
+box = box /10 ;
+}
+
+Console.WriteLine($"Сумма цифр в числе {number} = {sum}");
---
+++ homework04/example003/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+void printArray(int[] array, int quantity)
+{
+    for (int i = 0; i < quantity; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int[] creatArray(int[] array, int quantity)
+{
+    for (int i = 0; i < quantity; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+    return array;
+}
+Console.Write("Введите желаемое колличество элементов в массиве: ");
+int quantity = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[quantity];
+numbers = creatArray(numbers, quantity);
+printArray(numbers, quantity);
