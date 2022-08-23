a336db8beae5bc5234b9f29c9ccf7ac14e6b1371
allajka <allajka94@gmail.com>
2022-08-23 20:17:15 +0300

    homework06

--- homework06/example003/Program.cs
+++ homework06/example003/Program.cs
@@ -1,17 +1,16 @@
-﻿// Написать алгоритм поворота матрицы [N;N] на 90 градусов
+﻿/* Написать алгоритм поворота матрицы [N;N] на 90 градусов
+    012     012
+0   123  0  741    
+1   456  1  852
+2   789  2  963 */
 
 int[,] picture = new int[,]
 {
-    {0, 0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 0, 1, 0, 1, 0, 0, 0},
-    {0, 0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 0, 0, 1, 0},
-    {1, 0, 0, 0, 0, 0, 0, 0, 1},
-
+    {1, 0, 0, 0, 0},
+    {1, 0, 0, 0, 0},
+    {1, 0, 0, 0, 0},
+    {1, 0, 0, 0, 0},
+    {1, 1, 1, 1, 1},
 };
 
 void PrintPicture(int[,] arr)
@@ -28,4 +27,24 @@ void PrintPicture(int[,] arr)
 }
 
 PrintPicture(picture);
+System.Console.WriteLine();
 
+for (int i = 0; i < picture.GetLength(0); i++)
+{
+
+    // for (int j = 0; j < picture.GetLength(1); j++)
+    // {
+    //     int temporary = picture[i, j];
+    //     picture[i, j] = picture[j, picture.GetLength(1) - 1 - i];
+    //     picture[j, picture.GetLength(1) - 1 - i] = temporary;
+    // }
+    for (int j = 0; j < picture.GetLength(1); j++)
+    {
+        int temporary = picture[j, i];
+        picture[j, i] = picture[picture.GetLength(1) - 1 - i, j];
+        picture[picture.GetLength(1) - 1 - i, j] = temporary;
+    }
+}
+
+PrintPicture(picture);
+System.Console.WriteLine();
