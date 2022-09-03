156b9c51f628ddb1ffe5946d8e665f5ee9d91b47
allajka <allajka94@gmail.com>
2022-09-03 11:28:33 +0300

    homework09

--- homework09/example003/Program.cs
+++ homework09/example003/Program.cs
@@ -1,16 +1,42 @@
 ﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 m = 3, n = 2 -> A(m,n) = 29 */
 
-double m = 3;
-double n = 2;
-
-double Metod(double m, double n, double result = 0)
+/* Попытка решить самостоятельно без решения из Википедии
+double AckermanFunctions(double m, double n)
 {
-    if (m >= 3) return result += Math.Pow(2, n + 3) - 3;
-    else result += m * n + 1;
-    Console.WriteLine($"result: {result}");
-    if (m == 0) return result;
-    else return Metod(m - 1, n, result);
+    if (n == 0) return m + 1;
+    else if (n > 0 && m == 1) return n + 2;
+    else if (n > 0 && m == 2) return 2 * n + 3;
+    else if (n > 0 && m == 3) return Math.Pow(2, n + 3) - 3;
+    return -1; 
+}
+Console.WriteLine($" Значение A({m},{n}) = {AckermanFunctions(m, n)}"); */
+
+int AckermanFunctionsRecursion(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else
+    {
+        if (m != 0 && n == 0) return AckermanFunctionsRecursion(m - 1, 1);
+        else return AckermanFunctionsRecursion(m - 1, AckermanFunctionsRecursion(m, n - 1));
+    }
 }
 
-System.Console.WriteLine(Metod(m, n));
+int CheckInput(string text, int number = 0)
+{
+    while (true)
+    {
+        Console.Write($"{text}: ");
+        string input = Console.ReadLine()!;
+        bool flag = int.TryParse(input, out number);
+        if (flag && number >= 0) break;
+    }
+    return number;
+}
+
+Console.WriteLine("Программа, вычисления функции Аккермана A(m,n).");
+int m = CheckInput("Введите положительное число m: ");
+int n = CheckInput("Введите положительное число n: ");
+
+Console.WriteLine($" Значение A({m},{n}) = {AckermanFunctionsRecursion(m, n)}");
